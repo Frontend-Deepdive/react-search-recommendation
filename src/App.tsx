8b59e4663e172f75d9d3bbfeb5af50933e0e4466
silverain <ekdldkaa@naver.com>
2025-04-21 14:26:26 +0900

    feat : add search frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useAtom } from "jotai";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
+import { searchRecordAtom } from "./stores/atom";
+import { useEffect, useState } from "react";
 
 function App() {
+  // use 'useAtom' to read and write to the atom
+  const [searchRecords, setSearchRecords] = useAtom(searchRecordAtom);
+
+  const [currentSearchVal, setCurrentSearchVal] = useState("");
+
+  useEffect(() => {
+    console.log("currentSearchVal", currentSearchVal);
+  }, [currentSearchVal]);
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar val={currentSearchVal} setVal={setCurrentSearchVal} />
     </div>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,30 @@
-export default function SearchBar() {
+import { useState } from "react";
+
+interface SearchBarProps {
+  val: string;
+  setVal: (val: string) => void;
+}
+
+export default function SearchBar({ val, setVal }: SearchBarProps) {
+  const [searchVal, setSearchVal] = useState("");
+
   return (
     <>
-      <input type="text" placeholder="Search..." />
-      <button type="submit">Search</button>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setSearchVal(e.target.value)}
+        value={searchVal}
+      />
+      <button
+        type="submit"
+        onClick={() => {
+          setVal(searchVal);
+          setSearchVal("");
+        }}
+      >
+        Search
+      </button>
     </>
   );
 }
