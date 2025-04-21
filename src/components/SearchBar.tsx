91428d4e3a92d43baef0404868150fd00d8ae7fa
silverain <ekdldkaa@naver.com>
2025-04-21 14:38:19 +0900

    feat : 예외 처리

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -10,8 +10,15 @@ export default function SearchBar({ val, setVal }: SearchBarProps) {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // form 기본 동작 방지
+
+    // 예외 처리
+    if (searchVal === "") {
+      alert("검색어를 입력하세요");
+      return;
+    }
+
     setVal(searchVal);
-    setSearchVal("");
+    setSearchVal(""); // 검색어 초기화
   };
 
   return (
