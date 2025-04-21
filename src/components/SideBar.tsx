ddbc53158c9afa3196bdd7804a2287bf5d067687
silverain <ekdldkaa@naver.com>
2025-04-21 15:29:42 +0900

    fix : use atom in read mode

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,9 @@
-import { useAtom } from "jotai";
+import { useAtomValue } from "jotai";
 import { searchRecordAtom } from "../stores/atom";
 
 export default function SideBar() {
   // use 'useAtom' to read and write to the atom
-  const [searchRecords, setSearchRecords] = useAtom(searchRecordAtom);
+  const searchRecords = useAtomValue(searchRecordAtom);
   const reversedSearchRecords = [...searchRecords].reverse();
   return (
     <div>
