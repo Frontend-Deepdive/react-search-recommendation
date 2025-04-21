2a2f87338e54849e4efac2cf26178e36c0842f59
silverain <ekdldkaa@naver.com>
2025-04-21 14:46:35 +0900

    feat : 검색기록 저장 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,25 @@ import { useEffect, useState } from "react";
 
 function App() {
   // use 'useAtom' to read and write to the atom
-  const [searchRecords, setSearchRecords] = useAtom(searchRecordAtom);
+  const [searchRecords, setSearchRecords] = useAtom(searchRecordAtom); // stack
 
   const [currentSearchVal, setCurrentSearchVal] = useState("");
 
   useEffect(() => {
+    if (currentSearchVal === "") return; // 빈 문자열일 경우 실행하지 않음
     console.log("currentSearchVal", currentSearchVal);
+    if (searchRecords.includes(currentSearchVal)) {
+      // 중복된 경우 삭제
+      setSearchRecords((prev) =>
+        prev.filter((item) => item !== currentSearchVal)
+      );
+    }
+    setSearchRecords((prev) => [...prev, currentSearchVal]);
   }, [currentSearchVal]);
+
+  useEffect(() => {
+    console.log("searchRecords", searchRecords);
+  }, [searchRecords]);
 
   return (
     <div>
