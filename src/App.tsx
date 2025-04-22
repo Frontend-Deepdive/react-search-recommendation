9acc61d789917f826204178ba28107c9285e1aaf
abyss-s <lh3729@naver.com>
2025-04-22 14:44:29 +0900

    feat: 인풋 필드 컴포넌트 구현 및 App에 추가
    - 검색어 관리 스토어와 연결해 최근 검색어를 5개까지 저장할 수 있도록 수현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import InputField from './components/InputField/InputField';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className='flex gap-8'>
       <Sidebar />
+      <div className='flex flex-col gap-4'>
+        <InputField />
+      </div>
     </div>
   );
 }
