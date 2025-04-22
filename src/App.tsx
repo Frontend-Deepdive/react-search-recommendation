67805119ec35c5d52096255bed13819b2fcc1dfb
abyss-s <lh3729@naver.com>
2025-04-22 14:36:06 +0900

    feat: 사이드바 컴포넌트 구현 및 App에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Sidebar from './components/Sidebar/Sidebar';
+
 function App() {
-  return <div>this is me</div>;
+  return (
+    <div className='flex gap-8'>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
